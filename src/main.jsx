ba12f30a25b945125d0a84cd1d372d28d6dd6e25
unknown <theoladejitoluwa@gmail.com>
2025-07-08 16:06:53 +0100

    hello

--- src/main.jsx
+++ src/main.jsx
@@ -5,27 +5,12 @@ const root = createRoot(document.getElementById("root"));
 
 root.render(
   <>
+  <h1>Working</h1>
     <App
-      // setup="I got my daughter a fridge for her birthday."
       punchline="I can't wait to see her face light up when she opens it."
+      upvotes={10}
+      isPun={true}
     />
-    <App
-      setup="How did the hacker escape the police?"
-      punchline="He just ransomware!"
-    />
-    <App
-      setup="Why don't pirates travel on mountain roads?"
-      punchline="Scurvy."
-    />
-
-    <App
-      setup="Why do bees stay in the hive in the winter?"
-      punchline="Swarm."
-    />
-
-    <App
-      setup="What's the best thing about Switzerland?"
-      punchline="I don't know, but the flag is a big plus!"
-    />
+    
   </>
 );
